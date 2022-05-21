eec8d19d33519fa4e2eaecbe4ce76967f5f8930c
yjglab <yjgdesign@gmail.com>
2022-05-22 01:20:56 +0900

    Row, Col 반응형 적용

--- rt/front/components/AppLayout.js
+++ rt/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Input, Menu, Row, Col } from "antd";
 
 // index, profile, signup공통
 const AppLayout = ({ children }) => {
@@ -19,12 +19,31 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원되기</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://yjg-lab.tistory.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            yjglab
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };

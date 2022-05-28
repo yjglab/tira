fa4abc06651e9149685210b9ed021b665bf8a477
yjglab <yjgdesign@gmail.com>
2022-05-28 14:39:57 +0900

    redux devtools 히스토리 기록, enhancer추가

--- rt/front/store/configureStore.js
+++ rt/front/store/configureStore.js
@@ -1,10 +1,16 @@
 import { createWrapper } from "next-redux-wrapper";
-import { createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "../reducers";
 
 const configureStore = () => {
-  const store = createStore(reducer); // state + reducer
+  const middlewares = [];
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, enhancer); // state + reducer
   return store;
 };
 const wrapper = createWrapper(configureStore, {
